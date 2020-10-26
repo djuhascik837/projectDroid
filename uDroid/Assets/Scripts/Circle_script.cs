93488fe742e7198c822708a93b03ffa4f5735b35
JDornan06 <72090655+JDornan06@users.noreply.github.com>
2020-10-26 14:13:27 +0000

    Added basic On click functionality with some basic game objects

---
+++ uDroid/Assets/Scripts/Circle_script.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle_script : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("Circle");
+        Destroy(this.gameObject);
+    }
+}
---
+++ uDroid/Assets/Scripts/Triangle_script.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triangle_script : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("Triangle");
+        Destroy(this.gameObject);
+    }
+}
