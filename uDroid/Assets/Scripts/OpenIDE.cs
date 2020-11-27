08457b43ab205e78784d05d103236dcfe89f8f7e
40134602 <jdornan06@qub.ac.uk>
2020-11-27 21:08:03 +0000

    IDE animation menu done
    IDE animation menu done, added the fonts to the majority of text elements, created script for opening IDE panel -- Deleted unused TextMeshPro files

---
+++ uDroid/Assets/Scripts/OpenIDE.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenIDE : MonoBehaviour
+{
+    public GameObject idePanel;
+
+    public void StartIDEOpenAnim()
+    {
+        Animator anim = idePanel.GetComponent<Animator>();
+        if (anim != null)
+        {
+            bool isOpen = anim.GetBool("open");
+            anim.SetBool("open", !isOpen);
+        }
+    }
+
+}
