ea6fee9935c079138eca343601c776079aa06040
40134602 <jdornan06@qub.ac.uk>
2020-11-28 12:53:31 +0000

    Added basic user input for basic IDE test

---
+++ uDroid/Assets/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public InputField input;
+
+    public void GetInput(string userInput)
+    {
+        Debug.Log(userInput);
+        if(input != null)
+        {
+            input.text = "";
+        }
+    }
+}
