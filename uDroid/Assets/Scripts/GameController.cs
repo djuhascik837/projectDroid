3c2eadb39490fa2bbe8926863f5d0d5cfe081962
40134602 <jdornan06@qub.ac.uk>
2020-12-01 20:42:59 +0000

    Basic Input validation

---
+++ uDroid/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public InputField input;
+    public Text textFieldCompare;
+
+    public void GetInput(string userInput)
+    {
+        if(input != null)
+        {
+            string lowerCase = userInput.ToString().ToLower();
+            string text = textFieldCompare.text;
+
+            Debug.Log(lowerCase + ", " + text.ToLower());
+
+            if (text != null)
+            {
+                if (text.ToLower() == lowerCase) Debug.Log(lowerCase + ", " + text.ToLower());
+            }
+
+            input.text = "";
+        }
+    }
+}
