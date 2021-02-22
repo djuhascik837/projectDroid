824efae944c09eb02b92e44306e956749071f0c9
Jack Dornan <jdornan06@qub.ac.uk>
2021-02-22 13:17:25 +0000

    Added functionality for additional challenges
    Challenges still to be added

--- uDroid/Assets/Scripts/OpenIDE.cs
+++ uDroid/Assets/Scripts/OpenIDE.cs
@@ -5,15 +5,38 @@ using UnityEngine;
 public class OpenIDE : MonoBehaviour
 {
     public GameObject idePanel;
+    private CanvasGroup testPanel;
 
     public void StartIDEOpenAnim()
     {
         Animator anim = idePanel.GetComponent<Animator>();
+
         if (anim != null)
         {
             bool isOpen = anim.GetBool("open");
             anim.SetBool("open", !isOpen);
+        } else
+        {
+            Debug.LogWarning("Anim == null");
         }
     }
 
+    public void SelectRandomChallenge()
+    {
+        Debug.Log("starting Random Challenge");
+
+        testPanel = GameObject.Find("IDE - Panel").transform.Find("TextEditor - Panel Tester").GetComponent<CanvasGroup>();
+
+        if (testPanel.alpha != 0)
+        {
+            testPanel.alpha = 0;
+        }
+        else if (testPanel.alpha != 1)
+        {
+            testPanel.alpha = 1;
+        } else
+        {
+            Debug.LogWarning("Not 0 or 1");
+        }
+    }
 }
