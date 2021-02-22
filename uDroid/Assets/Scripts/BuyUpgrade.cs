2425e7722a2e37939daf7bdc93ea85ae97181403
40210520 <djuhascik01@qub.ac.uk>
2021-02-22 17:01:51 +0000

    Bug Fix - Slider Increase Independent
    Fixing bug where slider increase was only ever updating globally rather than per slider increase button.

--- uDroid/Assets/Scripts/BuyUpgrade.cs
+++ uDroid/Assets/Scripts/BuyUpgrade.cs
@@ -7,20 +7,24 @@ public class BuyUpgrade : MonoBehaviour
 {
     public UpgradePanel upgradePanel;
     public OpenMenuAnim openAnim;
-    public SliderIncrease sliderIncrease;
+    public SliderIncrease[] sliderIncrease;
+    public Button[] upgradeButton;
     public GameObject statusText;
     public GameObject statusBox;
     public GameObject autoCoin;
     public GameObject droidStats;
-    public GameObject[] plots;
     public double currentCoins;
     public static double upgradeValue = 4;
     public static bool autoCoinClicked = false;
     public static bool sliderMultiClicked = false;
     public static double numOfDroids;
     public static double coinsPerSec;
-    public double upgradeMultiplier = 1.5;
-    
+    public double upgradeMultiplier = 1.25;
+
+    private bool UpgradeClick1 = false;
+    private bool UpgradeClick2 = false;
+    private bool UpgradeClick3 = false;
+    private bool UpgradeClick4 = false;
 
 
     public void ClickUpgradeCoin()
@@ -38,6 +42,39 @@ public class BuyUpgrade : MonoBehaviour
         openAnim.press();
     }
 
+    public void setUpgradeBoolTrue(Slider slider)
+    {
+        if(slider.name == "Slider 1 - Slider")
+        {
+            UpgradeClick1 = true;
+            print("1" + UpgradeClick1);
+        } else if (slider.name == "Slider 2 - Slider")
+        {
+            UpgradeClick2 = true;
+            print("2" + UpgradeClick2);
+        }
+        else if (slider.name == "Slider 3 - Slider")
+        {
+            UpgradeClick3 = true;
+            print("3" + UpgradeClick3);
+        }
+        else if (slider.name == "Slider 4 - Slider")
+        {
+            UpgradeClick4 = true;
+            print("4" + UpgradeClick4);
+        }
+
+    }
+
+    public void setUpgradeBoolFalse()
+    {
+        UpgradeClick1 = false;
+        UpgradeClick2 = false;
+        UpgradeClick3 = false;
+        UpgradeClick4 = false;
+        print("Upgrades set to false.");
+    }
+
     private IEnumerator playAnim()
     {
 
@@ -50,15 +87,36 @@ public class BuyUpgrade : MonoBehaviour
             statusBox.GetComponent<Animation>().Play("StatusAnim");
             yield return new WaitForSeconds(0.25f);
             statusBox.SetActive(false);
-
+            setUpgradeBoolFalse();
         }
         else if (autoCoinClicked == true)
         {
             StartAutoCoin();
+            setUpgradeBoolFalse();
         }
         else if (sliderMultiClicked == true)
         {
-            SliderMultiplier();
+            if (UpgradeClick1 == true)
+            {
+                SliderMultiplier(sliderIncrease[0]);
+                UpgradeClick1 = false;
+            }
+            else if (UpgradeClick2 == true)
+            {
+                SliderMultiplier(sliderIncrease[1]);
+                UpgradeClick2 = false;
+            }
+            else if (UpgradeClick3 == true)
+            {
+                SliderMultiplier(sliderIncrease[2]);
+                UpgradeClick3 = false;
+            }
+            else if (UpgradeClick4 == true)
+            {
+                SliderMultiplier(sliderIncrease[3]);
+                UpgradeClick4 = false;
+            }
+
         }
     }
 
@@ -76,16 +134,12 @@ public class BuyUpgrade : MonoBehaviour
 
     }
 
-    public void SliderMultiplier()
+    public void SliderMultiplier(SliderIncrease sliderIncrease)
     {
         //This increases the time it takes for the slider to finish
         GlobalCoins.CoinCount -= upgradeValue;
         upgradeValue *= upgradeMultiplier;
-        if(sliderIncrease != null)
-        {
-            sliderIncrease.multiplier *= 1.25f;
-        }
-        
+        sliderIncrease.multiplier *= 1.7f;
         upgradePanel.upgradeSlider.SetActive(false);
     }
 
