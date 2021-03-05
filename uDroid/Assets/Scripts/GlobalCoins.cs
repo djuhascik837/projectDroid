4253ba836f07ebddb5b80b8ced12c345c32aabc4
40210520 <djuhascik01@qub.ac.uk>
2021-03-05 15:56:41 +0000

    Added current coin display to the upgrade Menu and fixced animation

--- uDroid/Assets/Scripts/GlobalCoins.cs
+++ uDroid/Assets/Scripts/GlobalCoins.cs
@@ -6,34 +6,40 @@ using UnityEngine.UI;
 public class GlobalCoins : MonoBehaviour
 {
     public static double CoinCount;
-    public GameObject CoinDisplay;
+    public GameObject[] CoinDisplay;
     public double InternalCoins;
 
     public void RoundCoins(double internalCoins)
     {
         if (InternalCoins >= 1000000000000000)
         {
-            CoinDisplay.GetComponent<Text>().text = (InternalCoins / 1000000000000000).ToString("0.#") + "Q";
+            CoinDisplay[0].GetComponent<Text>().text = (InternalCoins / 1000000000000000).ToString("0.#") + "Q";
+            CoinDisplay[1].GetComponent<Text>().text = (InternalCoins / 1000000000000000).ToString("0.#") + "Q";
         }
         else if (InternalCoins >= 1000000000000)
         {
-            CoinDisplay.GetComponent<Text>().text = (InternalCoins / 1000000000000).ToString("0.#") + "T";
+            CoinDisplay[0].GetComponent<Text>().text = (InternalCoins / 1000000000000).ToString("0.#") + "T";
+            CoinDisplay[1].GetComponent<Text>().text = (InternalCoins / 1000000000000).ToString("0.#") + "T";
         }
         else if (InternalCoins >= 1000000000)
         {
-            CoinDisplay.GetComponent<Text>().text = (InternalCoins / 1000000000).ToString("0.#") + "B";
+            CoinDisplay[0].GetComponent<Text>().text = (InternalCoins / 1000000000).ToString("0.#") + "B";
+            CoinDisplay[1].GetComponent<Text>().text = (InternalCoins / 1000000000).ToString("0.#") + "B";
         }
         else if (InternalCoins >= 1000000)
         {
-            CoinDisplay.GetComponent<Text>().text = (InternalCoins / 1000000).ToString("0.#") + "M";
+            CoinDisplay[0].GetComponent<Text>().text = (InternalCoins / 1000000).ToString("0.#") + "M";
+            CoinDisplay[1].GetComponent<Text>().text = (InternalCoins / 1000000).ToString("0.#") + "M";
         }
         else if (InternalCoins >= 10000)
         {
-            CoinDisplay.GetComponent<Text>().text = (InternalCoins / 1000).ToString("0.#") + "K";
+            CoinDisplay[0].GetComponent<Text>().text = (InternalCoins / 1000).ToString("0.#") + "K";
+            CoinDisplay[1].GetComponent<Text>().text = (InternalCoins / 1000).ToString("0.#") + "K";
         }
         else
         {
-            CoinDisplay.GetComponent<Text>().text = "" + InternalCoins;
+            CoinDisplay[0].GetComponent<Text>().text = InternalCoins.ToString("0.#");
+            CoinDisplay[1].GetComponent<Text>().text = InternalCoins.ToString("0.#");
         }
     }
 
