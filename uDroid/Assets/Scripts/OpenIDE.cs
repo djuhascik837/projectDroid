776eae516389db7784e2cfa21507eb1dded8bcc4
JDornan06 <jdornan06@qub.ac.uk>
2021-03-05 23:04:52 +0000

    Post code rework, basic testing complete
    Code seems to functioning correctly post code rework. Code blocks commented and left at the end of the class for documentation

--- uDroid/Assets/Scripts/OpenIDE.cs
+++ uDroid/Assets/Scripts/OpenIDE.cs
@@ -4,23 +4,20 @@ using UnityEngine.UI;
 public class OpenIDE : MonoBehaviour
 {
     public GameObject idePanel;
-    // -- to be removed
-    //private CanvasGroup testPanel;
-    //private CanvasGroup levelOne;
-    public Text text;
-    public Text textInput;
+    //// -- to be removed
+    ////private CanvasGroup testPanel;
+    ////private CanvasGroup levelOne;
+    //public Text text;
+    //public Text textInput;
     public GameObject[] challenges;
     public GameObject[] tutorials;
     public GameObject codingChallengePanel;
 
-    private bool isAnimOpen;
-    private static bool isActive = true;
     private static int tutLevel = 0;
 
     public void StartIDEOpenAnim()
     {
         Animator anim = idePanel.GetComponent<Animator>();
-        CanvasGroup[] canvasGroups = CreateCanvasGroupRef();
 
         if (anim != null)
         {
@@ -33,154 +30,28 @@ public class OpenIDE : MonoBehaviour
                     panel.SetActive(false);
                 }
 
-                foreach (var canvas in canvasGroups)
-                {
-                    canvas.alpha = 0;
-                }
-
                 foreach (var tutorial in tutorials)
                 {
                     tutorial.SetActive(false);
                 }
-
-                isActive = true;
                 HideCodingChallengePanel(false);
             }
 
             anim.SetBool("open", !isOpen);
-            isAnimOpen = isOpen;
         } else
         {
             Debug.LogWarning("Anim == null");
         }
     }
 
-    //Variable for Select Random Challenge
-    private readonly string strIdePanel = "IDE - Panel";
-    private readonly string level2 = "Level 2 - Basic For Loop";
-    private readonly string level1 = "Level 1 - Basic If";
 
     // -- to be removed
+    //Variable for Select Random Challenge
+    //private readonly string strIdePanel = "IDE - Panel";
+    //private readonly string level2 = "Level 2 - Basic For Loop";
+    //private readonly string level1 = "Level 1 - Basic If";
     //private readonly string tutHelloWorld = "Tutorial - HelloWorld";
     //private readonly string tutHWCodeConsole = "CodeConsole";
-
-    public void SelectRandomChallenge()
-    {
-        Debug.Log("starting Random Challenge");
-        HideCodingChallengePanel(true);
-
-        CanvasGroup[] canvasGroups = CreateCanvasGroupRef();
-        string ideEditor = "IDE Editor - Input Field";
-
-        InputField level2Field = GameObject.Find(strIdePanel).transform.Find(level2).Find(ideEditor).GetComponent<InputField>();
-        InputField level1Field = GameObject.Find(strIdePanel).transform.Find(level1).Find(ideEditor).GetComponent<InputField>();
-        // -- to be removed
-        //InputField tutHelloWorldField = GameObject.Find(strIdePanel).transform.Find(tutHelloWorld).Find(tutHWCodeConsole).Find(ideEditor).GetComponent<InputField>();
-
-        if (isActive)
-        {
-            int randSwitcher = RandomNumber(0, canvasGroups.Length);
-            isActive = false;
-
-            if (randSwitcher == 0)
-            {
-                if (!isAnimOpen)
-                {
-                    CanvasGroup testPanel = null;
-
-                    foreach (var panel in challenges)
-                    {
-                        if (panel.name.Equals(level2))
-                        {
-                            testPanel = panel.GetComponent<CanvasGroup>();
-                            panel.SetActive(true);
-                        }
-                    }
-
-                    GameController.input = level2Field;
-                    GameController.textFieldCompare = "Test Panel Test";
-
-                    testPanel.alpha = 1;
-                    //CanvasAlphaSetter(testPanel);
-                }
-                else
-                {
-                    foreach (var canvas in canvasGroups)
-                    {
-                        canvas.alpha = 0;
-                    }
-                }
-            }
-            else if (randSwitcher == 1)
-            {
-                if (!isAnimOpen)
-                {
-                    CanvasGroup levelOne = null;
-
-                    foreach (var panel in challenges)
-                    {
-                        if (panel.name.Equals(level1))
-                        {
-                            levelOne = panel.GetComponent<CanvasGroup>();
-                            panel.SetActive(true);
-                        }
-                    }
-
-                    GameController.input = level1Field;
-                    GameController.textFieldCompare = "Level One Test";
-
-                    levelOne.alpha = 1;
-                    //CanvasAlphaSetter(levelOne);
-                }
-                else
-                {
-                    foreach (var canvas in canvasGroups)
-                    {
-                        canvas.alpha = 0;
-                    }
-                }
-            }
-            //Testing purposed -- remove later
-            //else if (randSwitcher == 2)
-            //{
-            //    if (!isAnimOpen)
-            //    {
-            //        CanvasGroup tutHelloWorldCanvas = null;
-
-            //        foreach (var panel in panels)
-            //        {
-            //            if (panel.name.Equals(tutHelloWorld))
-            //            {
-            //                tutHelloWorldCanvas = panel.GetComponent<CanvasGroup>();
-            //                panel.SetActive(true);
-            //            }
-            //        }
-
-            //        GameController.input = tutHelloWorldField;
-            //        GameController.textFieldCompare = "Hello World";
-
-            //        tutHelloWorldCanvas.alpha = 1;
-            //        //CanvasAlphaSetter(levelOne);
-            //    }
-            //    else
-            //    {
-            //        foreach (var canvas in canvasGroups)
-            //        {
-            //            canvas.alpha = 0;
-            //        }
-            //    }
-            //}
-        }
-        else
-        {
-            foreach (var panel in challenges)
-            {
-                panel.SetActive(false);
-            }
-            isActive = true;
-        }
-    }
-
 
     public void StartRandomChallenge()
     {
@@ -192,6 +63,12 @@ public class OpenIDE : MonoBehaviour
         switch (rand)
         {
             case 0:
+                HideCodingChallengePanel(true);
+                SetTutorialsOrChallengesInactive(challenges, rand);
+                break;
+            case 1:
+                HideCodingChallengePanel(true);
+                SetTutorialsOrChallengesInactive(challenges, rand);
                 break;
             default:
                 Debug.LogWarning("Challenges rand defaulted");
@@ -216,7 +93,7 @@ public class OpenIDE : MonoBehaviour
             case 0: // Hello World
                 print("starting first tutorial");
                 HideCodingChallengePanel(true);
-                SetTutorialsInactive(tutorials, tutLevel);
+                SetTutorialsOrChallengesInactive(tutorials, tutLevel);
 
                 GameController.input = tutorialOneInputFields[0];
                 GameController.textFieldCompare = "Hello World";
@@ -226,7 +103,7 @@ public class OpenIDE : MonoBehaviour
             case 1: // Variables
                 print("starting second tutorial");
                 HideCodingChallengePanel(true);
-                SetTutorialsInactive(tutorials, tutLevel);
+                SetTutorialsOrChallengesInactive(tutorials, tutLevel);
 
                 GameController.input = tutorialTwoInputFields[0];
                 GameController.textFieldCompare = "Hello World";
@@ -236,7 +113,7 @@ public class OpenIDE : MonoBehaviour
             case 2: // Comments
                 print("starting third tutorial");
                 HideCodingChallengePanel(true);
-                SetTutorialsInactive(tutorials, tutLevel);
+                SetTutorialsOrChallengesInactive(tutorials, tutLevel);
 
                 GameController.input = tutorialThreeInputFields[0];
 
@@ -248,7 +125,7 @@ public class OpenIDE : MonoBehaviour
             case 3: // Casting
                 print("starting fourth tutorial");
                 HideCodingChallengePanel(true);
-                SetTutorialsInactive(tutorials, tutLevel);
+                SetTutorialsOrChallengesInactive(tutorials, tutLevel);
 
                 GameController.input = tutorialFourInputFields[0];
 
@@ -260,7 +137,7 @@ public class OpenIDE : MonoBehaviour
             case 4: // Operators
                 print("starting fifth tutorial");
                 HideCodingChallengePanel(true);
-                SetTutorialsInactive(tutorials, tutLevel);
+                SetTutorialsOrChallengesInactive(tutorials, tutLevel);
 
                 GameController.input = tutorialFiveInputFields[0];
                 print("need to work out multiple input fields actually -- not gonna work as i thought");
@@ -272,7 +149,7 @@ public class OpenIDE : MonoBehaviour
             case 5: // Maths
                 print("starting sixth tutorial");
                 HideCodingChallengePanel(true);
-                SetTutorialsInactive(tutorials, tutLevel);
+                SetTutorialsOrChallengesInactive(tutorials, tutLevel);
 
                 GameController.input = tutorialSixInputFields[0];
                 print("need to work out multiple input fields actually -- not gonna work as i thought");
@@ -284,7 +161,7 @@ public class OpenIDE : MonoBehaviour
             case 6: // Methods
                 print("starting seventh tutorial");
                 HideCodingChallengePanel(true);
-                SetTutorialsInactive(tutorials, tutLevel);
+                SetTutorialsOrChallengesInactive(tutorials, tutLevel);
 
                 GameController.input = tutorialSevenInputFields[0];
                 Debug.LogWarning("!!! Add Text field compare string, currently: Hello World");
@@ -302,7 +179,7 @@ public class OpenIDE : MonoBehaviour
         }
     }
 
-    private void SetTutorialsInactive(GameObject[] tutorials, int ignore)
+    private void SetTutorialsOrChallengesInactive(GameObject[] tutorials, int ignore)
     {
         for (int i = 0; i < tutorials.Length; i++)
         {
@@ -341,27 +218,117 @@ public class OpenIDE : MonoBehaviour
         System.Random random = new System.Random();
         return random.Next(min, max);
     }
-
-    // To be Removed as it started causing issues - after changing code to include tutorial button found this was the issue
-    //private void CanvasAlphaSetter(CanvasGroup canvasGroup)
-    //{
-    //    if (canvasGroup.alpha != 0)
-    //    {
-    //        canvasGroup.alpha = 0;
-    //    }
-    //    else if (canvasGroup.alpha != 1)
-    //    {
-    //        canvasGroup.alpha = 1;
-    //    }
-    //}
-
-    public CanvasGroup[] CreateCanvasGroupRef()
-    {
-        CanvasGroup[] canvasGroups = new CanvasGroup[2];
-        canvasGroups[0] = GameObject.Find(strIdePanel).transform.Find(level2).GetComponent<CanvasGroup>();
-        canvasGroups[1] = GameObject.Find(strIdePanel).transform.Find(level1).GetComponent<CanvasGroup>();
-        //canvasGroups[2] = GameObject.Find(strIdePanel).transform.Find(tutHelloWorld).GetComponent<CanvasGroup>();
-
-        return canvasGroups;
-    }
 }
+
+// --- all to be removed -- leaving for documentation
+
+//public CanvasGroup[] CreateCanvasGroupRef()
+//{
+//    CanvasGroup[] canvasGroups = new CanvasGroup[2];
+//    canvasGroups[0] = GameObject.Find(strIdePanel).transform.Find(level2).GetComponent<CanvasGroup>();
+//    canvasGroups[1] = GameObject.Find(strIdePanel).transform.Find(level1).GetComponent<CanvasGroup>();
+//    //canvasGroups[2] = GameObject.Find(strIdePanel).transform.Find(tutHelloWorld).GetComponent<CanvasGroup>();
+
+//    return canvasGroups;
+//}
+
+// To be Removed as it started causing issues - after changing code to include tutorial button found this was the issue
+//private void CanvasAlphaSetter(CanvasGroup canvasGroup)
+//{
+//    if (canvasGroup.alpha != 0)
+//    {
+//        canvasGroup.alpha = 0;
+//    }
+//    else if (canvasGroup.alpha != 1)
+//    {
+//        canvasGroup.alpha = 1;
+//    }
+//}
+
+
+//public void SelectRandomChallenge()
+//{
+//    Debug.Log("starting Random Challenge");
+//    HideCodingChallengePanel(true);
+
+//    CanvasGroup[] canvasGroups = CreateCanvasGroupRef();
+//    string ideEditor = "IDE Editor - Input Field";
+
+//    InputField level2Field = GameObject.Find(strIdePanel).transform.Find(level2).Find(ideEditor).GetComponent<InputField>();
+//    InputField level1Field = GameObject.Find(strIdePanel).transform.Find(level1).Find(ideEditor).GetComponent<InputField>();
+//    // -- to be removed
+//    //InputField tutHelloWorldField = GameObject.Find(strIdePanel).transform.Find(tutHelloWorld).Find(tutHWCodeConsole).Find(ideEditor).GetComponent<InputField>();
+
+//    if (isActive)
+//    {
+//        int randSwitcher = RandomNumber(0, canvasGroups.Length);
+//        isActive = false;
+
+//        if (randSwitcher == 0)
+//        {
+//            if (!isAnimOpen)
+//            {
+//                CanvasGroup testPanel = null;
+
+//                foreach (var panel in challenges)
+//                {
+//                    if (panel.name.Equals(level2))
+//                    {
+//                        testPanel = panel.GetComponent<CanvasGroup>();
+//                        panel.SetActive(true);
+//                    }
+//                }
+
+//                GameController.input = level2Field;
+//                GameController.textFieldCompare = "Test Panel Test";
+
+//                testPanel.alpha = 1;
+//                //CanvasAlphaSetter(testPanel);
+//            }
+//            else
+//            {
+//                foreach (var canvas in canvasGroups)
+//                {
+//                    canvas.alpha = 0;
+//                }
+//            }
+//        }
+//        else if (randSwitcher == 1)
+//        {
+//            if (!isAnimOpen)
+//            {
+//                CanvasGroup levelOne = null;
+
+//                foreach (var panel in challenges)
+//                {
+//                    if (panel.name.Equals(level1))
+//                    {
+//                        levelOne = panel.GetComponent<CanvasGroup>();
+//                        panel.SetActive(true);
+//                    }
+//                }
+
+//                GameController.input = level1Field;
+//                GameController.textFieldCompare = "Level One Test";
+
+//                levelOne.alpha = 1;
+//                //CanvasAlphaSetter(levelOne);
+//            }
+//            else
+//            {
+//                foreach (var canvas in canvasGroups)
+//                {
+//                    canvas.alpha = 0;
+//                }
+//            }
+//        }
+//    }
+//    else
+//    {
+//        foreach (var panel in challenges)
+//        {
+//            panel.SetActive(false);
+//        }
+//        isActive = true;
+//    }
+//}
