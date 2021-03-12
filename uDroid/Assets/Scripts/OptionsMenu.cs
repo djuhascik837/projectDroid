ee3f3dfbdb2bf8d9c40647fe60acf388b14d6b24
JDornan06 <jdornan06@qub.ac.uk>
2021-03-12 15:45:36 +0000

    Minor bug fix

--- uDroid/Assets/Scripts/OptionsMenu.cs
+++ uDroid/Assets/Scripts/OptionsMenu.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class OptionsMenu : MonoBehaviour
 {
     public GameObject optionsMenu;
+    public GameObject[] runScriptParticles;
 
     public void SetVisible(bool hide)
     {
-        print(hide);
         if(hide)
         {
             optionsMenu.SetActive(true);
+
+            foreach (var particle in runScriptParticles)
+            {
+                particle.SetActive(false);
+            }
         }
         else
         {
             optionsMenu.SetActive(false);
+
+            foreach (var particle in runScriptParticles)
+            {
+                particle.SetActive(true);
+            }
         }
     }
 }
