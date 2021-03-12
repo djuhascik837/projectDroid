6f0f22b556f0bbc25296546f5f885259fd774848
JDornan06 <jdornan06@qub.ac.uk>
2021-03-12 14:59:26 +0000

    Added Options Menu to main Level

--- uDroid/Assets/Scripts/VolumeValueChange.cs
+++ uDroid/Assets/Scripts/VolumeValueChange.cs
@@ -27,10 +27,10 @@ public class VolumeValueChange : MonoBehaviour
 
     public void MuteSound()
     {
-
+        musicVolume = 0.001f;
     }
     public void unMuteSound()
     {
-
+        musicVolume = 1f;
     }
 }
